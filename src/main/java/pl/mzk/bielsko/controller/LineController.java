65afff4258b7f6d7ff16333681b4e289c68ddae0
Michal <m.ski@poczta.fm>
2017-12-01 23:43:09 +0100

    version-0.75

--- src/main/java/pl/mzk/bielsko/controller/LineController.java
+++ src/main/java/pl/mzk/bielsko/controller/LineController.java
@@ -2,8 +2,10 @@ package pl.mzk.bielsko.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.mzk.bielsko.model.Line;
 import pl.mzk.bielsko.service.LineService;
 
 import java.util.Map;
@@ -18,5 +20,53 @@ public class LineController {
     public String linesList(Map<String,Object> map){
         map.put("lineList", lineService.getAllLines());
         return "lines";
+    }
+
+    @RequestMapping(value = "/editLine/{id}", method = RequestMethod.GET)
+    public String edit(@PathVariable("id") int lineId, Map<String,Object> map){
+        //Stop stop = stopService.findStop(stopId);
+        Line line =lineService.findLine(lineId);
+        map.put("line", line);
+        return "editLine";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(Line line, Map<String,Object> map){
+        //stopService.updateStop(stop);
+        lineService.updateLine(line);
+        return "redirect:lineDetails" + line.getLineId(); //zabezpiecza przed podwojnym kliknieciem
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String delete(@PathVariable("id") int lineId, Map<String,Object> map){
+        //stopService.deleteStop(stopId);
+        lineService.deleteLine(lineId);
+        return "redirect:lines";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String register(Map<String,Object> map){
+        //map.put("line", new Stop());
+        map.put("line", new Line());
+        return "createLine";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(Line line, Map<String,Object> map){
+        //stopService.createStop(line);
+        lineService.createLine(line);
+        return "redirect:lineDetails" + line.getLineId(); //zabezpiecza przed podwojnym kliknieciem
+    }
+
+    @RequestMapping(value = "/details/{id}", method = RequestMethod.GET)
+    public String details(@PathVariable("id") int lineId, Map<String,Object> map){
+        Line line = lineService.findLine(lineId);
+        map.put("lineId", line.getLineId());
+        map.put("lineNumber", line.getLineNumber());
+        map.put("relation", line.getRelation());
+        map.put("direction", line.getDirection());
+        map.put("validFrom", line.getValidFrom());
+        map.put("validTo", line.getValidTo());
+        return "lineDetails";
     }
 }
--- src/main/java/pl/mzk/bielsko/controller/StopController.java
+++ src/main/java/pl/mzk/bielsko/controller/StopController.java
@@ -2,8 +2,10 @@ package pl.mzk.bielsko.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.mzk.bielsko.model.Stop;
 import pl.mzk.bielsko.service.StopService;
 
 import java.util.Map;
@@ -19,4 +21,48 @@ public class StopController {
         map.put("stopList", stopService.getAllStops());
         return "stops";
     }
+
+    @RequestMapping(value = "/editStop/{id}", method = RequestMethod.GET)
+    public String edit(@PathVariable("id") int stopId, Map<String,Object> map){
+        Stop stop = stopService.findStop(stopId);
+        map.put("stop", stop);
+        return "editStop";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(Stop stop, Map<String,Object> map){
+        stopService.updateStop(stop);
+        return "redirect:stopDetails" + stop.getStopId(); //zabezpiecza przed podwójnym kliknięciem
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String delete(@PathVariable("id") int stopId, Map<String,Object> map){
+        stopService.deleteStop(stopId);
+        return "redirect:stops";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String register(Map<String,Object> map){
+        map.put("stop", new Stop());
+        return "createStop";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(Stop stop, Map<String,Object> map){
+        stopService.createStop(stop);
+        return "redirect:stopDetails" + stop.getStopId(); //zabezpiecza przed podwójnym kliknięciem
+    }
+
+    @RequestMapping(value = "/details/{id}", method = RequestMethod.GET)
+    public String details(@PathVariable("id") int stopId, Map<String,Object> map){
+        Stop stop = stopService.findStop(stopId);
+        map.put("stopId", stop.getStopId());
+        map.put("street", stop.getStreet());
+        map.put("stopName", stop.getStopName());
+        map.put("lineNumber", stop.getLineNumber());
+        map.put("direction", stop.getDirection());
+        map.put("departTime", stop.getDepartTime());
+        return "stopDetails";
+    }
+
 }
